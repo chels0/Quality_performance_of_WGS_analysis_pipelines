07e7b8bc452cff758f63b8aac42a92d23e43289f
chels0 <chelso97@gmail.com>
2022-02-28 09:18:00 +0100

    fixed error

--- scripts/remove_contaminants.py
+++ scripts/remove_contaminants.py
@@ -8,19 +8,23 @@ Created on Tue Feb  8 13:21:38 2022
 import sys
 
 fasta_name = sys.argv[1]
-contig_size_to_remove = int(sys.argv[2])
+contig_size_to_remove = sys.argv[2]
 
 with open(fasta_name+'.fasta', "r") as f:
     seq = f.readlines()
 
 every_other = seq[1::2]
-hej = len(every_other[0])
 to_change = seq.copy()
+
+test = []
     
 for i in range(len(every_other)):
+    change = i
+    change += change + 1
+    test.append(change)
     if len(every_other[i]) < contig_size_to_remove:
-        to_change[i+2] = 0
-        to_change[i+1] = 0
+        to_change[change] = 0
+        to_change[change-1] = 0
     
 no_int = [x for x in to_change if not isinstance(x,int)]
 
