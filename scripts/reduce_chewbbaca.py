133125242205e54a2430ab8269d827fcbfcf4533
Chelsea Ramsin <chelso97@gmail.com>
2022-03-22 09:17:45 +0100

    small changes

--- scripts/reduce_chewbbaca.py
+++ scripts/reduce_chewbbaca.py
@@ -23,7 +23,7 @@ for filename in os.listdir(directory+'/chewbbaca_quast_tables/'):
     list_of_files.append(filename.split('.')[0]) #split on . to separate filename from file extension
 
 combos = [] #list of combinations of filenames
-  
+
 #loop for creating all pairwise combinations of filenames
 for L in range(2, 3):
     for subset in itertools.combinations(list_of_files, L):
@@ -39,10 +39,11 @@ columns = ['Sample','# contigs', 'Largest contig', 'Total length', 'Reference le
 for files in combos:
     filename1 = files[0] #name of first combination of files
     filename2 = files[1] # name of second combination of files
- 
+    
     #create dataframes
     df = pd.read_csv(directory+'/chewbbaca_quast_tables/'+filename1+'.tsv', sep='\t')
     df2 = pd.read_csv(directory+'/chewbbaca_quast_tables/'+filename2+'.tsv', sep= '\t')
+   
     
     indices = []
     indices2 = []
@@ -59,7 +60,7 @@ for files in combos:
             indices.append(indices2[i][0])
         #otherwise only add first 3 indices of indices2 list
         else:
-            indices.append(indices2[i][0]+'_'+indices2[i][1]+'_'+indices2[i][2])
+            indices.append(indices2[i][0]+'_'+indices2[i][1])
     
     #set elements of indices as index
     df.index = indices 
@@ -70,7 +71,7 @@ for files in combos:
     df2_chewbbaca = df2.drop(columns, axis=1)
     
     #extract differences between two chewbbaca dataframes
-    test2 = df_chewbbaca.compare(df2_chewbbaca, align_axis=1).rename(columns={'self': filename, 'other': filename2}, level=-1)
+    test2 = df_chewbbaca.compare(df2_chewbbaca, align_axis=1).rename(columns={'self': filename1, 'other': filename2}, level=-1)
     test2 = test2.fillna('')
     
     #extract quast columns to new dataframe
@@ -82,26 +83,30 @@ for files in combos:
     df2_quast = df2_quast.drop('Sample', axis=1)
     
     #define filenames for csv table
-    filename3 = filename.split('_results')
+    filename3 = filename1.split('_results')
     filename4 = filename2.split('_results')
-    
+ 
     #save chewbbaca comparisons
-    test2.to_csv(filename3[0]+'_vs_'+filename4[1]+'chewbbaca.tsv', sep='\t', encoding='utf-8')
+    #test2.to_csv(filename3[0]+'_vs_'+filename4[1]+'chewbbaca.tsv', sep='\t', encoding='utf-8')
     
     #extract differences between two quast dataframes
     test3 = df_quast.compare(df2_quast, align_axis=1).rename(columns={'self': filename, 'other': filename2}, level=-1)
     
     #save quast dataframes
-    test3.to_csv(filename3[0]+'_vs_'+filename4[0]+'_quast.tsv', sep='\t', encoding='utf-8')
+    #test3.to_csv(filename3[0]+'_vs_'+filename4[0]+'_quast.tsv', sep='\t', encoding='utf-8')
     
     #merge quast and chewbbaca results
-    merged = pd.merge(test3,test2,left_index= True, right_index=True, how="outer")
-    merged = merged.fillna('')
-    merged.sort_index(inplace=True, ascending=False)
+    #merged = pd.merge(test3,test2,left_index= True, right_index=True, how="outer")
+    #merged = merged.fillna('')
+    #merged.sort_index(inplace=True, ascending=False)
+    
+    comp = df.compare(df2, align_axis=1).rename(columns={'self': filename1, 'other': filename2}, level=-1)
+    comp = comp.fillna('')
+    
     
     #save to csv
     pathlib.Path('Results/Comparisons').mkdir(parents=True, exist_ok=True)
-    merged.to_csv('Results/Comparisons/'+ filename3[0]+'_vs_'+filename4[0]+'_results.tsv', sep='\t', encoding='utf-8')
+    comp.to_csv('Results/Comparisons/'+ filename3[0]+'_vs_'+filename4[0]+'.tsv', sep='\t', encoding='utf-8')
 
 # #test3 = pd.concat([df_quast, df2_quast], keys=[filename, filename2], axis=1, sort=False)
 
